7fd9d6b2a566847de6c75f66ac8fd1f0cc09b9b5
Ahmet Yücel Reisoğlu <73395715+TurkishRebel@users.noreply.github.com>
2022-06-24 14:47:23 +0300

    Update Pencereİşlevli.py

--- Pencereİşlevli.py
+++ Pencereİşlevli.py
@@ -6,15 +6,24 @@ class Pencere(QtWidgets.QWidget):
     def __init__(self):
         super().__init__()
         self.init_ui()
+        
 
     def init_ui(self):
+        self.gir = QtWidgets.QLineEdit()
         self.yazı_alanı = QtWidgets.QLabel("Bana henüz tıklanmadı")
         self.buton = QtWidgets.QPushButton("Bana tıkla")
+        self.buton2 = QtWidgets.QPushButton("Temizle")
+        self.buton3 = QtWidgets.QPushButton("Bas")
         self.say = 0
 
         v_box = QtWidgets.QVBoxLayout()
+        
+        v_box.addWidget(self.gir)
         v_box.addWidget(self.yazı_alanı)
-        v_box.addWidget(self.buton)
+        v_box.addWidget(self.buton)        
+        v_box.addWidget(self.buton2)
+        v_box.addWidget(self.buton3)
+
         v_box.addStretch()
 
         h_box = QtWidgets.QHBoxLayout()
@@ -22,9 +31,13 @@ class Pencere(QtWidgets.QWidget):
         h_box.addStretch()
         h_box.addLayout(v_box)
         h_box.addStretch()
-
+        
+        self.setLayout(v_box)
         self.setLayout(h_box)
         self.buton.clicked.connect(self.click)
+        self.buton2.clicked.connect(self.fonk)
+        self.buton3.clicked.connect(self.fonk2)
+
         self.show()
 
     def click(self):
@@ -33,7 +46,13 @@ class Pencere(QtWidgets.QWidget):
 
         self.show()
 
+    def fonk(self):
+        self.gir.clear()
+    def fonk2(self):
+        self.yazı_alanı.setText("AAAAAAA")
 
+    
+       
 app = QtWidgets.QApplication(sys.argv)
 pencere = Pencere()
 sys.exit(app.exec_())
